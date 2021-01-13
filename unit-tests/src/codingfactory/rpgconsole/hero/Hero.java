4935b70b97da24a62a603ad71afeeefba9b5f9b5
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2021-01-13 15:05:16 +0100

    add comments in previous test and class

--- unit-tests/src/codingfactory/rpgconsole/hero/Hero.java
+++ unit-tests/src/codingfactory/rpgconsole/hero/Hero.java
@@ -47,35 +47,52 @@ public class Hero {
     }
     //endregion
 
-
     // region setters / covered
     private void setLevel(Integer newLevel) {
         this.level = newLevel;
     }
+
     private void setHp(Integer newHp) {
         this.hp = newHp;
     }
+
     private void setHpMax(Integer newHpMax) {
         this.hpMax = newHpMax;
     }
+
     private void setAtk(Integer newAtk) {
         this.atk = newAtk;
     }
+
     private void setXp(Integer newXp) {
         this.xp = newXp;
     }
     // endregion
 
+    /**
+     * decrease hero's hp depending on received damage
+     * @param damage => inflicted damage's amount
+     */
     public void takeDamage(Integer damage){
         this.setHp(this.getHp() - damage);
     }
 
+    /**
+     * attack an enemy and inflict damage to it
+     * @param enemy => enemy under hero attack
+     */
     public void attack(Enemy enemy) {
         Random rand = new Random();
         int randomNum = rand.nextInt(this.level + 1);
         enemy.takeDamage(this.atk+randomNum); //enemy take damage for hero attack
     }
 
+    /**
+     * increase hero's XP.
+     * If hero's XP > hpmax * level, hero levelup
+     * @param addedXp => received XP amount
+     * @see #levelUp()
+     */
     public void increaseXp(int addedXp) {
         this.setXp(this.getXp() + addedXp);
         if (this.getXp() > this.getHpMax() * this.getLevel()){
@@ -83,6 +100,10 @@ public class Hero {
         }
     }
 
+    /**
+     * increase hero's level.
+     * He gain +1 atk, +3 Hp, and XP go down to 0.
+     */
     public void levelUp() {
         this.setXp(0);
         this.setAtk(this.getAtk()+1);
